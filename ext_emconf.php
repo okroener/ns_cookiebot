20db3224dee7ba17d0dfea8f49918f0b4f50d9f5
NITSAN Technologies <sanjay@nitsan.in>
2019-05-25 12:30:30 +0530

    Automatic Code Review with Travis CI, Adding Contributors and Minor Bug Fixing

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,18 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Cookiebot.com TYPO3 GDPR/EPR Compliant',
     'description' => 'Quickly configure Cookiebot.com to your TYPO3 site to helps make your use of cookies and online tracking GDPR and EPR compliant',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies',
-    'author_email' => 'sanjay@nitsan.in',
+    'author' => 'T3:Milan Rathod, T3:Keval Pandya, QA:Nisha Dave',
+	'author_email' => 'sanjay@nitsan.in',
+	'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '6.0.0-9.5.99',
