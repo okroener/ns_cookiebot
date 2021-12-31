978011f4a45f71b36d4923c5c2dc86012edf8a79
NITSAN Technologies <sanjay@nitsan.in>
2021-12-31 17:19:43 +0530

    [TASK] Final review and ready to release v2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_cookiebot'] = [
     'title' => '[NITSAN] Cookiebot.com GDPR/EPR Compliant',
     'description' => 'Quickly configure Cookiebot.com to your TYPO3 site to helps make your use of cookies and online tracking GDPR and EPR compliant. LIVE-demo: https://demo.t3terminal.com/t3t-extensions/cookiebot PRO-version: https://t3terminal.com/cookiebot-gdpr-compliant-typo3-extension-free',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-11.9.99',
+            'typo3' => '6.0.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
