5a0ab226c5013df9bf640726507d6c464da19a2c
Oliver Kroener <ok@oliver-kroener.de>
2023-05-26 07:19:53 +0200

    Added support for Typo3 12.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3_MODE') || defined('TYPO3') or die();
 $extKey = 'ns_cookiebot';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', '[NITSAN] Cookiebot');
