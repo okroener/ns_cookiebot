714516d9b54fafd3b6f0e87c09b7ada087c09f6a
NITSAN Technologies <sanjay@nitsan.in>
2023-06-30 19:46:02 +0530

    [TASK] Change TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF['ns_cookiebot'] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.0.0-12.9.99',
+            'typo3' => '6.0.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
